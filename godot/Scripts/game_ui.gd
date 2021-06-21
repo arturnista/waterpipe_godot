80168bfc2410a13a0a8611d12def6efafe1be587
Artur Morelle Nista <arturnista@gmail.com>
2021-06-21 20:37:54 -0300

    sounds

--- godot/Scripts/game_ui.gd
+++ godot/Scripts/game_ui.gd
@@ -4,9 +4,11 @@ var room_name = ""
 var websocket = null
 
 func _ready():
+	get_node("/root/UI_SFX/MenuMusic").stop()
 	room_name = get_node("RoomData/RoomName")
 	room_name.text = "Room code: " + connection.room_name
 	websocket = get_node("../Connection")
 
 func _on_DisconnectButton_pressed():
 	websocket.close_connection()
+	get_node("/root/UI_SFX/MenuMusic").play(0)
